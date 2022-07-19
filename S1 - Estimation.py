bb933599d07c88d64a37f82a7b6e6aa44fb8efec
Théo Lopès-Quintas <63822750+theo-lq@users.noreply.github.com>
2022-07-19 17:22:11 +0200

    Update S1 - Estimation.py

--- S1 - Estimation.py
+++ S1 - Estimation.py
@@ -4,6 +4,8 @@ from random import randint
 mean = lambda vector : sum(vector) / len(vector)
 
 
+
+
 def game():
     dice_roll = randint(1, 6)
     gain = dice_roll
@@ -11,6 +13,8 @@ def game():
         dice_roll = randint(1, 6)
         gain += dice_roll
     return gain
+
+
 
 
 def estimation(game, times=10000):
